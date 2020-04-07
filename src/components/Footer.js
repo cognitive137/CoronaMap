388574b24a3655d5fe98c8602034f48cd74ab852
Sana Habeeb <sana.alia.habeeb@gmail.com>
2020-04-08 03:00:04 +0530

    Initial commit.

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Container from "components/Container";
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <p>
+          &copy; {new Date().getFullYear()},
+          Built with Gatsby.
+        </p>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
